8a639541cfa83f72c79aa22ea0ac09f41b22aba2
vladislav <dincrash@yandex.ru>
2019-12-16 13:53:50 +0300

    change fire rate bullet

--- src/ActionPage.java
+++ src/ActionPage.java
@@ -4,50 +4,29 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
-
-import java.awt.*;
 import java.util.ArrayList;
 
 public class ActionPage extends BasicGameState {
 
     public static final int ID = 1;
-
-
     Image background = null;
     Image img = null;
     Image hostile = null;
     Image bullet = null;
     private ArrayList<Bullet> bulletList = new ArrayList<Bullet>();
-
-    /**
-     * True if space is down
-     */
     private boolean w;
     private boolean s;
-    /**
-     * True if left shift is down
-     */
     private boolean a;
-    /**
-     * True if right shift is down
-     */
     private boolean d;
     private AppGameContainer app;
-
     private float v;
-    /**
-     * The y position of our controlled stuff
-     */
     private float b;
     private boolean g;
-    /**
-     * The input syste being polled
-     */
     private Input input;
     int x;
     int y;
-    int time=0;
-
+    private static int default_bullet_delay = 500;
+    private static int time = 0;
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
         background = new Image("resources/background.png");
@@ -57,7 +36,6 @@ public class ActionPage extends BasicGameState {
         if (gameContainer instanceof AppGameContainer) {
             app = (AppGameContainer) gameContainer;
         }
-
         input = gameContainer.getInput();
         v = 300;
         b = 300;
@@ -65,11 +43,6 @@ public class ActionPage extends BasicGameState {
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-
-//        g.drawLine(300, 300, 400 , 400);
-//        g.setColor(Color.red);
-//        g.drawString("Sabre!", 250, 10);
-//        g.drawString("Шо", 250, 250);
         graphics.drawImage(background, 0, 0);
         img.draw(v, b, 128, 128);
         hostile.draw(500, 500, 128, 128);
@@ -78,11 +51,9 @@ public class ActionPage extends BasicGameState {
         graphics.drawString("a: " + a, 100, 280);
         graphics.drawString("d: " + d, 100, 300);
 //        bullet.draw(100,100);
-
         graphics.setColor(Color.red);
         for (int i = 0; i < bulletList.size(); i++) {
             Bullet bullet = bulletList.get(i);
-
             try {
                 bullet.move();
                 graphics.fillRect(bullet.location.x, bullet.location.y, 5, 5);
@@ -110,26 +81,21 @@ public class ActionPage extends BasicGameState {
         if (gameContainer.getInput().isKeyDown(Input.KEY_D)) {
             v = v + 5;
         }
-
         if (input.isKeyPressed(input.KEY_F1)) {
-
             stateBasedGame.enterState(0);
         }
         if (input.isKeyPressed(input.KEY_F3)) {
-
             stateBasedGame.enterState(2);
         }
-        time += i;
 
-        if (gameContainer.getInput().isMouseButtonDown(input.MOUSE_LEFT_BUTTON)) {
+        if(input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+            time -= i;
 
-            addNewBullet(input.getMouseX(), input.getMouseY());
-
+            if (time <= 0) {
+                addNewBullet(input.getMouseX(), input.getMouseY());
+                time = default_bullet_delay;
+            }
         }
-
-    }
-
-    private void addNewBullet(Bullet bullet) {
     }
 
     private void addNewBullet(int x, int y) {
@@ -139,7 +105,6 @@ public class ActionPage extends BasicGameState {
             e.printStackTrace();
         }
     }
-
 
     @Override
     public int getID() {
